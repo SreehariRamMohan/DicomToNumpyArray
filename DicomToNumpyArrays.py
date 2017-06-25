07b6258ac38fc21ea70c73793b6191e123a3fbb3
sreeharirammohan@gmail.com <wizmathhelp@gmail.com>
2017-06-24 20:38:25 -0700

    Initial Commit
    This saves a folder of .dcm to numpy arrays in a list called ArrayDicom

---
+++ DicomToNumpyArrays.py
@@ -0,0 +1,42 @@
+import dicom
+import os
+import numpy
+
+PathDicom = "/path/to/dicom/files"
+lstFilesDCM = []  # create an empty list
+count = 0
+for dirName, subdirList, fileList in os.walk(PathDicom):
+    for filename in fileList:
+        if ".dcm" in filename.lower():  # check whether the file's DICOM
+            lstFilesDCM.append(os.path.join(dirName,filename))
+            count += 1
+            print("Dicom Number :" + str(count))
+            
+# Get ref file
+RefDs = dicom.read_file(lstFilesDCM[0])
+
+# Load dimensions based on the number of rows, columns, and slices (along the Z axis)
+ConstPixelDims = (int(RefDs.Rows), int(RefDs.Columns), len(lstFilesDCM))
+
+# Load spacing values (in mm)
+ConstPixelSpacing = (float(RefDs.PixelSpacing[0]), float(RefDs.PixelSpacing[1]), float(RefDs.SliceThickness))
+
+# The array is sized based on 'ConstPixelDims'
+ArrayDicom = numpy.zeros(ConstPixelDims, dtype=RefDs.pixel_array.dtype)
+
+# loop through all the DICOM files
+for filenameDCM in lstFilesDCM:
+    # read the file
+    ds = dicom.read_file(filenameDCM)
+    # store the raw image data
+    ArrayDicom[:, :, lstFilesDCM.index(filenameDCM)] = ds.pixel_array
+
+for arr in ArrayDicom:
+    print(arr)
+    print("Printing NumpyArray")
+
+print("Done")
+
+
+
+
